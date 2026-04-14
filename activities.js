22d1124926a174c43ae0e9e04b9c8f0058d5cf97
nicoEPGV <nicolas.gousset@comite-epgv.fr>
2026-04-14 20:24:21 +0200

    Add activities configuration in activities.js
    This file defines various activities with their labels, presets, and editable zones.

---
+++ activities.js
@@ -0,0 +1,81 @@
+export const ACTIVITIES = {
+
+  yoga: {
+    label: "Yoga",
+    presets: { SOUP: 4, RENFO: 2, CARDIO: 0, EQUI: 3 },
+    editableZones: ["RENFO", "EQUI"]
+  },
+
+  stretching: {
+    label: "Stretching",
+    presets: { SOUP: 5, RENFO: 0, CARDIO: 0, EQUI: 2 },
+    editableZones: []
+  },
+
+  body_zen: {
+    label: "Body Zen",
+    presets: { SOUP: 3, RENFO: 2, CARDIO: 0, EQUI: 3 },
+    editableZones: ["EQUI", "RENFO"]
+  },
+
+  autre_seance_souplesse: {
+    label: "Autre séance Souplesse",
+    presets: { SOUP: 3, RENFO: 0, CARDIO: 0, EQUI: 0 },
+    editableZones: ["EQUI", "RENFO"]
+  },
+
+  programme_diabete_surpoids: {
+    label: "Programme Diabète & Surpoids",
+    presets: { SOUP: 2, RENFO: 3, CARDIO: 3, EQUI: 2 },
+    editableZones: ["EQUI", "RENFO", "SOUP"]
+  },
+
+  seance_multi_activites_bien_vieillir: {
+    label: "Séance Multi activités Bien vieillir",
+    presets: { SOUP: 2, RENFO: 3, CARDIO: 4, EQUI: 2 },
+    editableZones: ["RENFO", "EQUI", "CARDIO", "SOUP"]
+  },
+
+  programme_gym_apres_cancer: {
+    label: "Programme Gym’Après Cancer®",
+    presets: { SOUP: 2, RENFO: 2, CARDIO: 2, EQUI: 2 },
+    editableZones: ["RENFO", "EQUI", "CARDIO", "SOUP"]
+  },
+
+  programme_gymmemoire: {
+    label: "Programme Gymmémoire®",
+    presets: { SOUP: 2, RENFO: 2, CARDIO: 2, EQUI: 2 },
+    editableZones: ["RENFO", "EQUI", "CARDIO", "SOUP"]
+  },
+
+  seance_multi_activites: {
+    label: "Séance Multi activités",
+    presets: { SOUP: 2, RENFO: 2, CARDIO: 2, EQUI: 2 },
+    editableZones: ["RENFO", "EQUI", "CARDIO", "SOUP"]
+  },
+
+  seance_multi_activites_exterieur: {
+    label: "Séance Multi Activités Exterieur",
+    presets: { SOUP: 2, RENFO: 2, CARDIO: 2, EQUI: 2 },
+    editableZones: ["RENFO", "EQUI", "CARDIO", "SOUP"]
+  },
+
+  seance_corps_et_memoire: {
+    label: "Séance Corps et mémoire",
+    presets: { SOUP: 2, RENFO: 2, CARDIO: 2, EQUI: 2 },
+    editableZones: ["RENFO", "CARDIO", "SOUP"]
+  },
+
+  seance_apa_multi_pathologies: {
+    label: "Séance APA multi-pathologies",
+    presets: { SOUP: 2, RENFO: 2, CARDIO: 2, EQUI: 2 },
+    editableZones: ["RENFO", "EQUI", "CARDIO", "SOUP"]
+  },
+
+  seance_7_12_ans: {
+    label: "Séance 7 – 12 ans",
+    presets: { SOUP: 1, RENFO: 2, CARDIO: 2, EQUI: 1 },
+    editableZones: ["RENFO", "EQUI", "CARDIO", "SOUP"]
+  }
+
+};
